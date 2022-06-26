b13ea9021d5ce271b4ebcc4b558821e2b7cbb6aa
Samantha Wolff <samanthawolff07@gmail.com>
2022-06-26 17:24:15 -0500

    create Modal component

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Modal from '../Modal';
 
 
 const PhotoList = ({ category }) => {
@@ -40,12 +41,14 @@ const PhotoList = ({ category }) => {
   
     return (
       <div>
+        <Modal />
         <div className="flex-row">
           {currentPhotos.map((image, i) => (
             <img
               src={require(`../../assets/portfolio-img/${category}/${i}.jpg`)}
               alt={image.name}
               className="img-thumbnail mx-1"
+              onClick={() => toggleModal(image, i)}
               key={image.name}
             />
           ))}
