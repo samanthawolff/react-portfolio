e7b6f373acb992c1f72038ff6623159e3ec0dff8
Samantha Wolff <samanthawolff07@gmail.com>
2022-06-30 20:19:21 -0500

    change background and add avatar

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import coverImage from '../../assets/cover/cover-img.jpeg';
+import ProfilePic from '../../assets/profile/bitmoji-pic.png';
 
 
 function About() {
     return (
-        <section className="my-5">
-            <img src={coverImage} className="coverphoto" style={{ width: '100%' }} alt='cover' />
+        <section className="">
             <h1 id="about">About Me</h1>
+            <img src={ProfilePic} alt="Samantha Avatar" className="profile-pic"></img>
+
         </section>
     );
 }
